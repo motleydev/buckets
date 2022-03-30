c37e9e5ce12638ebd16f0ed75a6025b9909e8e87
Jesse Martin <themotleydev@gmail.com>
2022-03-30 12:16:10 +0200

    test

--- www/lib/urql.ts
+++ www/lib/urql.ts
@@ -24,7 +24,7 @@ const createUrqlClient = (data: any) => {
   });
   return createClient({
     exchanges: [dedupExchange, cacheExchange, ssr, fetchExchange],
-    url: process.env.NEXT_PUBLIC_HASURA_PROJECT_ENDPOINT as string,
+    url: process.env.HASURA_PROJECT_ENDPOINT as string,
   });
 };
 
